25816f59aa80ff35598cf8ef75871fc8eb28023c
Hitesh Navare <navarehitesh65@gmail.com>
2025-03-21 15:00:18 +0530

    added user signup route

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,40 @@
 import express from "express";
-const app = express();
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+import {UserModel} from "./db";
 
-app.get
+const app = express();
+app.use(express.json());
+
+//@ts-ignore
+app.post("/user/signup", async (req, res)=>{
+    const {username, password} = req.body;
+
+    const alreadyUser = await UserModel.findOne({username})
+
+        if(alreadyUser){
+            return res.status(400).send("you are already signed up")
+        }
+
+    const hashedpassword =await bcrypt.hash(password, 10)
+
+    await UserModel.create({
+        username: username,
+        password: hashedpassword
+    })
+    res.status(200).json({message: "you are signed successfully"})
+})
+
+app.post("/user/signup", (req , res)=>{
+    const {username, password} = req.body;
+
+    if(!username || !password){
+        res.status(400).json({
+            message: "Both fields are Required"
+        })
+    }
+
+   
+
+
+})
