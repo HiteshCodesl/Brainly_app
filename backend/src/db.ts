26d5930d5f9b5fc4d458e539075091a35ffc96dc
Hitesh Navare <navarehitesh65@gmail.com>
2025-03-23 15:25:38 +0530

    added content schema

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,12 +1,21 @@
-import mongoose, {Schema, model} from "mongoose"
+import mongoose, {Schema, model, mongo} from "mongoose"
 mongoose.connect("mongodb+srv://hiteshnavare:crGLGRwEizgVSncx@cluster0.j1065.mongodb.net/Brainly")
 
 const UserSchema = new Schema({
     username: {type: String, required: true, unique: true},
     password: {type: String, required: true},
 })
-
 export const UserModel = model("User",UserSchema )
+
+
+const ContentSchema = new Schema({
+    title: String,
+    link: String,
+    tags:[{type: mongoose.Types.ObjectId, ref: "Tag"}],
+    userId: {type: mongoose.Types.ObjectId, ref: "User", required: true}
+})
+export const contentModel = model("content", ContentSchema)
+
 
 const tagSchema = new Schema({
     title: {type: String, required: true,
